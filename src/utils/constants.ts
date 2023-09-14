5098ba5632425418afaab03e71674b1dbbac9ec2
Michal Bella <40443047+michalbella@users.noreply.github.com>
2023-09-14 09:03:30 +0200

    added correct discord link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -199,7 +199,7 @@ export const MAX_TICKET_MATCHES = 10
 export const MAX_TICKETS = 5
 
 export const SOCIAL_LINKS = {
-	DISCORD: 'https://discord.com/invite/7zzg2Y5s',
+	DISCORD: 'https://discord.gg/EU2HcptM',
 	TWITTER: 'https://twitter.com/sponglyio',
 	TERMS: 'https://overtimemarkets.xyz/static/media/thales-terms-of-use.9f69e9a2.pdf'
 }
