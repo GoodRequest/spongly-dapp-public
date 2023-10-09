c3bfffbe113ad816c3e4b05725f1b1409fa51b74
JakubCvacho <jakub.cvacho@goodrequest.com>
2023-10-09 10:15:37 +0200

    CH-FIX-INDEXES

---
+++ src/pages/leaderboard/index.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from 'next'
+import Layout from '@/layout/layout/Layout'
+import SEOHelmet from '@/atoms/SEOHelmet'
+import { IPage } from '@/typescript/types'
+import LeaderboardContent from '@/content/leaderboardContent/LeaderboardContent'
+
+export const getStaticProps: GetStaticProps<{ page: IPage<{}> }> = async () => {
+	const page = {
+		data: {},
+		// TODO: currently not translated. should be added when languageswitcher is implemented.
+		// TODO: doplnit leaderboard seo tags
+		shareTags: {
+			title: 'Leaderboard - Take direct control of your tickets on our platform and witness live match results',
+			description: `Explore Spongly's My Wallet page for efficient management of your sports market tickets. Experience the innovation of decentralized ticket copying from elite players, and conveniently track and manage your current tickets. Join now to elevate your sports engagement and witness game-changing results!`
+		}
+	}
+	if (!page) return { notFound: true }
+
+	return {
+		props: {
+			page
+		}
+	}
+}
+
+const LeaderboardPage = (props: any) => <LeaderboardContent {...props} />
+
+LeaderboardPage.getLayout = function getLayout(page: any) {
+	return (
+		<>
+			<SEOHelmet shareTags={page.props?.page?.shareTags} />
+			<Layout>{page}</Layout>
+		</>
+	)
+}
+export default LeaderboardPage
---
+++ src/pages/parlay-superstars/index.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from 'next'
+import Layout from '@/layout/layout/Layout'
+import SEOHelmet from '@/atoms/SEOHelmet'
+import { IPage } from '@/typescript/types'
+import ParlayLeaderboardContent from '@/content/ParlayLeaderboardContent/ParlayLeaderboardContent'
+
+export const getStaticProps: GetStaticProps<{ page: IPage<{}> }> = async () => {
+	const page = {
+		data: {},
+		// TODO: currently not translated. should be added when languageswitcher is implemented.
+		shareTags: {
+			title: 'Parlay Superstars - Elevate Your Game with Decentralized Ticket Replication',
+			description:
+				'Discover the power of the spongly.eth.limo Parlay Superstars, where top-performing players with exceptional success rates shine. Compare, analyze, and seamlessly copy their tickets in the decentralized sports market. Join now to revolutionize your sports engagement and amplify your chances of success!'
+		}
+	}
+	if (!page) return { notFound: true }
+
+	return {
+		props: {
+			page
+		}
+	}
+}
+const ParlaySuperstarsPage = (props: any) => <ParlayLeaderboardContent {...props} />
+
+ParlaySuperstarsPage.getLayout = function getLayout(page: any) {
+	return (
+		<>
+			<SEOHelmet shareTags={page.props?.page?.shareTags} />
+			<Layout>{page}</Layout>
+		</>
+	)
+}
+
+export default ParlaySuperstarsPage
