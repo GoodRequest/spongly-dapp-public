640228a4da6e08086f0431b31a06467d81bdb9a5
Daniel Hlavaty <daniel.hlavaty@goodrequest.com>
2023-10-24 10:58:55 +0200

    CH-529

---
+++ src/pages/match-detail/index.tsx
@@ -0,0 +1,38 @@
+import { GetStaticProps } from 'next'
+import React from 'react'
+import Layout from '@/layout/layout/Layout'
+import SEOHelmet from '@/atoms/SEOHelmet'
+import { IPage } from '@/typescript/types'
+import MatchDetailContent from '@/content/matchesContent/MatchDetailContent'
+
+export const getStaticProps: GetStaticProps<{ page: IPage<{}> }> = async () => {
+	const page = {
+		data: {},
+		// TODO: currently not translated. should be added when languageswitcher is implemented.
+		shareTags: {
+			title: 'Matches - Decentralized Ticket Copying for Ultimate Sports Market Success',
+			description:
+				'Dive into Spongly’s Matches feature, where decentralized sports market innovation meets ticket replication from the finest players. Amplify your sports engagement by mirroring top-tier strategies. Join us now to redefine the way you experience sports!'
+		}
+	}
+	if (!page) return { notFound: true }
+
+	return {
+		props: {
+			page
+		}
+	}
+}
+
+const MatchDetailPage = (props: any) => <MatchDetailContent {...props} />
+
+MatchDetailPage.getLayout = function getLayout(page: any) {
+	return (
+		<>
+			<SEOHelmet shareTags={page?.props?.page?.shareTags} />
+			<Layout>{page}</Layout>
+		</>
+	)
+}
+
+export default MatchDetailPage
